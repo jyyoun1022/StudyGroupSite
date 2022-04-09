f0ed2e18522a06e27270bfc409eb234579e3c542
choi <csj21300@naver.com>
2022-04-09 23:13:56 +0900

    init

---
+++ src/test/java/com/studygroupsite/StudyGroupSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studygroupsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyGroupSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
