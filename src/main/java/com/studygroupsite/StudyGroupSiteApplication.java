56d614442c7fd742ea6fa00e9a8d37445bbc2a3e
choi <csj21300@naver.com>
2022-04-09 23:47:09 +0900

    init

---
+++ src/main/java/com/studygroupsite/StudyGroupSiteApplication.java
@@ -0,0 +1,11 @@
+package com.studygroupsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyGroupSiteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StudyGroupSiteApplication.class, args);
+    }
+}
