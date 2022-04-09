6f6b93a46bbccca47863794e8d29d2c4499ae5aa
choi <csj21300@naver.com>
2022-04-10 02:56:52 +0900

    init

--- src/main/java/com/studygroupsite/controller/TestController.java
+++ src/main/java/com/studygroupsite/controller/TestController.java
@@ -1,14 +1,17 @@
 package com.studygroupsite.controller;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Log4j2
 @RestController
 @RequestMapping("/test")
 public class TestController {
     @GetMapping(value="")
     public String test() {
+        log.info("test");
         return "TEST COMPLETE";
     }
 }
